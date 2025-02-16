36f10fe2b437e982aa36d09fdb0ac5ad548908b6
Katie Sim <katie.sim@csuglobal.edu>
2025-02-16 17:57:36 -0600

    first commit

---
+++ src/m1proj/character.java
@@ -0,0 +1,19 @@
+package m1proj;
+
+public class character {
+
+	public static void main(String[] args) {
+String firstName = "Sherlock";
+String lastName = "Holmes";
+String address = "221B Baker Street";
+String city = "London";
+String zipCode = "NW1 6XE";
+
+System.out.println("First Name: " + firstName);
+System.out.println("Last Name: " + lastName);
+System.out.println("Street Address: " + address);
+System.out.println("City: " + city);
+System.out.println("Postal Code: " + zipCode);
+	}
+
+}
